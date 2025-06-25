bdad3581de90957d0c377a2f3fc96cd37bce3ec2
Mohit <bluesky5april2021@gmail.com>
2025-06-26 05:16:12 +0530

    added home, error, docs pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DocsComponent } from './docs/docs.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "docs", component: DocsComponent},
+    {path: "**", component: ErrorComponent}
+];
